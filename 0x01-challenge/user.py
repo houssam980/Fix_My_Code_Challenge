5303aeba389808d39881712c5b496990c6a46d17
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-07 08:19:14 -0800

    fixing

---
+++ 0x01-challenge/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class User:
+    def __init__(self):
+        self.__email = None
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        if not isinstance(value, str):
+            raise TypeError("email must be a string")
+        self.__email = value
+
+
+if __name__ == "__main__":
+    u = User()
+    u.email = "john@snow.com"
+    print(u.email)
+
